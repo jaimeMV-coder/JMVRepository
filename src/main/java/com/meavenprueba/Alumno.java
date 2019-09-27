f52c9f132f5263dbd03c5c38b679ee6e78d52ebd
User <User@DESKTOP-8STGO5H>
2019-09-27 13:14:17 -0500

    Projecto Prueba Maven

---
+++ src/main/java/com/meavenprueba/Alumno.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.meavenprueba;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author User
+ */
+@Entity
+@Table(name = "alumno")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Alumno.findAll", query = "SELECT a FROM Alumno a"),
+    @NamedQuery(name = "Alumno.findByUserId", query = "SELECT a FROM Alumno a WHERE a.userId = :userId"),
+    @NamedQuery(name = "Alumno.findByUsername", query = "SELECT a FROM Alumno a WHERE a.username = :username")})
+public class Alumno implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "user_id")
+    private Integer userId;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "username")
+    private String username;
+
+    public Alumno() {
+    }
+
+    public Alumno(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Alumno(Integer userId, String username) {
+        this.userId = userId;
+        this.username = username;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (userId != null ? userId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Alumno)) {
+            return false;
+        }
+        Alumno other = (Alumno) object;
+        if ((this.userId == null && other.userId != null) || (this.userId != null && !this.userId.equals(other.userId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.meavenprueba.Alumno[ userId=" + userId + " ]";
+    }
+    
+}
