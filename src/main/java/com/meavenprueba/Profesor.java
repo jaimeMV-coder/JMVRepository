8111ba33f009106b1b345dbfcd58f26ed243a34a
User <User@DESKTOP-8STGO5H>
2019-09-27 15:43:30 -0500

    Se añade metodo DAO a la BRANCH

---
+++ src/main/java/com/meavenprueba/Profesor.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.meavenprueba;
+
+/**
+ *
+ * @author User
+ */
+public class Profesor {
+    private Integer profesor_id;
+    private String username;
+    private Integer alumno_id;
+
+    public Integer getProfesor_id() {
+        return profesor_id;
+    }
+
+    public void setProfesor_id(Integer profesor_id) {
+        this.profesor_id = profesor_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAlumno_id() {
+        return alumno_id;
+    }
+
+    public void setAlumno_id(Integer alumno_id) {
+        this.alumno_id = alumno_id;
+    }
+    
+}
